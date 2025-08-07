9673db389884488e3d3a46e49208f6033e7fd2b8
Belal Mohamed <ibelalmohammed@gmail.com>
2025-08-07 15:29:37 +0300

    Update File

---
+++ Day6Lab/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Day6Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Day6 Part1
+            /*
+          1 - Object Class                                                   Done
+          2 - Inheritance                                                    Done
+          3 - Polymorphism  Overload - Override                              Done
+                       Method overriding  virtual ,override ,new             Done
+
+          How to Open IL Disassembly in VS CLR C# -- MSIL --> JIT --> 0101   Done
+          ---------------------------------
+          SDK Framework
+          Open this path C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools
+          Open File ilasm.exe
+          Drag and Drop (exe Or dll) File to show the IL Dasm Code For it 
+
+          Recursion Method                                                   Done
+
+          Types Of Class  (Referenace Type)                                  Done
+          ***************
+          Normal Class                                                       Done
+          Sealed Class                                                       Done
+          static Class                                                       Done
+          Partial Class                                                      Done
+          Abstract Class                                                     Done
+          [Inner]Internal Class}                                             Done
+          Is                                                                 Done
+          --------------------------------------------------
+           Extension Method                                                  Done
+          4 - Encapsulation  DLL  (Hide Data)  Property Full Auto            Done
+          5 - Access Modifiers                                               Done                    
+              -----------------
+              https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/access-modifiers
+
+                  (public ,private ,protected,internal,protected internal, private protected ,file)
+
+                  public: Code in any assembly can access this type or member. The accessibility level of the containing type controls the accessibility level of public members of the type.
+                  private: Only code declared in the same class or struct can access this member.
+                  protected: Only code in the same class or in a derived class can access this type or member.
+                  internal: Only code in the same assembly can access this type or member.
+                  protected internal: Only code in the same assembly or in a derived class in another assembly can access this type or member.
+                  private protected: Only code in the same assembly and in the same class or a derived class can access the type or member.
+                  file: Only code in the same file can access the type or member.
+          */
+
+            #endregion
+        }
+    }
+}
